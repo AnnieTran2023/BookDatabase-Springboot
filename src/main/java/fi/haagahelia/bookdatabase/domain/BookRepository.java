78da9ea70ab10a457891fe4b8996d3da77889d99
Annie Tran <tranthilananh105@gmail.com>
2024-09-22 21:55:24 +0300

    rest service for finding a book by title

--- src/main/java/fi/haagahelia/bookdatabase/domain/BookRepository.java
+++ src/main/java/fi/haagahelia/bookdatabase/domain/BookRepository.java
@@ -1,8 +1,8 @@
 package fi.haagahelia.bookdatabase.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
-public interface BookRepository extends CrudRepository <Book, Long> {
-
-    
-} 
+public interface BookRepository extends CrudRepository<Book, Long> {
+    List<Book> findByTitle(String title);
+}

0cb5ecc84770fa89526a0ea65f7d49a0c3a86ee5
Annie Tran <tranthilananh105@gmail.com>
2024-09-26 15:14:10 +0300

    Create the login page and add login method to controller

--- src/main/java/fi/haagahelia/bookdatabase/web/BookController.java
+++ src/main/java/fi/haagahelia/bookdatabase/web/BookController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,11 @@ public class BookController {
 
     @Autowired
     private CategoryRepository crepository;
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 
     @RequestMapping(value = { "/", "/booklist" })
     public String bookList(Model model) {

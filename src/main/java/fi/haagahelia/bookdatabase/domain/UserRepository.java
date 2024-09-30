34e06d7ef01494f63d2da0b07e60bc55a88816ed
Annie Tran <tranthilananh105@gmail.com>
2024-09-30 16:33:46 +0300

    update UserRepository

--- src/main/java/fi/haagahelia/bookdatabase/domain/UserRepository.java
+++ src/main/java/fi/haagahelia/bookdatabase/domain/UserRepository.java
@@ -1,10 +1,9 @@
 package fi.haagahelia.bookdatabase.domain;
 
 import org.springframework.data.repository.CrudRepository;
-import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    List<User> findByUsername(String username);
+    User findByUsername(String username);
 
 }

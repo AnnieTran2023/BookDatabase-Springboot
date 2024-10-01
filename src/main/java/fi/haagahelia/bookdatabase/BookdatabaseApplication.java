93556088ed8c8e941f79167983f36bc97fa10fe3
Annie Tran <tranthilananh105@gmail.com>
2024-10-01 15:42:11 +0300

    edit roles of sample users

--- src/main/java/fi/haagahelia/bookdatabase/BookdatabaseApplication.java
+++ src/main/java/fi/haagahelia/bookdatabase/BookdatabaseApplication.java
@@ -12,31 +12,37 @@ import fi.haagahelia.bookdatabase.domain.BookRepository;
 import fi.haagahelia.bookdatabase.domain.Category;
 import fi.haagahelia.bookdatabase.domain.CategoryRepository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 public class BookdatabaseApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(BookdatabaseApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookdatabaseApplication.class, args);
 	}
 
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository, AppUserRepository urepository) {
+	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository,
+			AppUserRepository urepository) {
 		return (args) -> {
 			// Creating categories
 			Category fiction = new Category("Fiction");
 			Category scienceFiction = new Category("Science Fiction");
 			Category mystery = new Category("Mystery");
-			
 
 			// Saving categories to the repository
 			crepository.save(fiction);
 			crepository.save(scienceFiction);
 			crepository.save(mystery);
 
-			//Create users
-			AppUser user1 = new AppUser("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER","user@email.fi");
+			// Create users
+			AppUser user1 = new AppUser("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "ROLE_USER",
+					"user@email.fi");
 			AppUser user2 = new AppUser("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C",
-					"ADMIN", "admin@email.fi");
+					"ROLE_ADMIN", "admin@email.fi");
 
 			urepository.save(user1);
 			urepository.save(user2);
@@ -53,6 +59,8 @@ public class BookdatabaseApplication {
 			repository.save(book2);
 			repository.save(book3);
 			repository.save(book4);
+			log.info(user1.getRole());
+			log.info(user2.getRole());
 		};
 	}
 }

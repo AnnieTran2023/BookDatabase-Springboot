133db31dc7a8bb292d1df20fa560fed5c3f58329
Annie Tran <tranthilananh105@gmail.com>
2025-03-27 10:09:06 +0200

    sample users

--- src/main/java/fi/haagahelia/bookdatabase/BookdatabaseApplication.java
+++ src/main/java/fi/haagahelia/bookdatabase/BookdatabaseApplication.java
@@ -28,39 +28,38 @@ public class BookdatabaseApplication {
 	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository,
 			AppUserRepository urepository) {
 		return (args) -> {
-			// Creating categories
-			// Category fiction = new Category("Fiction");
-			// Category scienceFiction = new Category("Science Fiction");
-			// Category mystery = new Category("Mystery");
+	
+			Category fiction = new Category("Fiction");
+			Category scienceFiction = new Category("Science Fiction");
+			Category mystery = new Category("Mystery");
 
-			// // Saving categories to the repository
-			// crepository.save(fiction);
-			// crepository.save(scienceFiction);
-			// crepository.save(mystery);
+			// Saving categories to the repository
+			crepository.save(fiction);
+			crepository.save(scienceFiction);
+			crepository.save(mystery);
 
-			// Create users
-			// AppUser user1 = new AppUser("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER",
-			// 		"user@email.fi");
-			// AppUser user2 = new AppUser("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C",
-			// 		"ADMIN", "admin@email.fi");
+		
+			AppUser user1 = new AppUser("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER",
+					"user@email.fi");
+			AppUser user2 = new AppUser("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C",
+					"ADMIN", "admin@email.fi");
 
-			// urepository.save(user1);
-			// urepository.save(user2);
+			urepository.save(user1);
+			urepository.save(user2);
 
-			// Creating books and associating them with categories
-			// Book book1 = new Book("JK Rowling", "Harry Potter", "978-1-4088-1234-5", 2023, fiction);
-			// Book book2 = new Book("Isaac Asimov", "Foundation", "978-0-553-08002-7", 2021, scienceFiction);
-			// Book book3 = new Book("Agatha Christie", "Murder on the Orient Express", "978-0-06-207350-1", 2020,
-			// 		mystery);
-			// Book book4 = new Book("George R.R. Martin", "A Game of Thrones", "978-0-553-10354-0", 1999, fiction);
+			Book book1 = new Book("JK Rowling", "Harry Potter", "978-1-4088-1234-5", 2023, fiction);
+			Book book2 = new Book("Isaac Asimov", "Foundation", "978-0-553-08002-7", 2021, scienceFiction);
+			Book book3 = new Book("Agatha Christie", "Murder on the Orient Express", "978-0-06-207350-1", 2020,
+					mystery);
+			Book book4 = new Book("George R.R. Martin", "A Game of Thrones", "978-0-553-10354-0", 1999, fiction);
 
-			// // Saving books to the repository
-			// repository.save(book1);
-			// repository.save(book2);
-			// repository.save(book3);
-			// repository.save(book4);
-			// log.info(user1.getRole());
-			// log.info(user2.getRole());
+			// Saving books to the repository
+			repository.save(book1);
+			repository.save(book2);
+			repository.save(book3);
+			repository.save(book4);
+			log.info(user1.getRole());
+			log.info(user2.getRole());
 		};
 	}
 }
